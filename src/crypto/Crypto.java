6bcbf5e6f0348bbba4db62bc14631c75951a0d0c
Z0064FP6 <Z0064FP6@PC014972.pmt.primetals.net>
2018-12-09 16:26:28 +0100

    zzzz

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -59,6 +59,7 @@ public class Crypto {
     keyStorePassword = password;
   }
   
+  // test
   //============================================================================================================================================
 
   public static boolean isPasswordCorrect() {
